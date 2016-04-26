8d6ace22a4221ca0f717c3b73896c2f2c2da4b31
a9650615 <a9650615@gmail.com>
2016-04-26 20:33:55 +0800

    fix no login bug

--- js/setting.js
+++ js/setting.js
@@ -1,7 +1,7 @@
 
 var exports = module.exports = {};
 
-//exports.UserCookies = 'igneous=01d33ef3fdeb06d9cdc324f6b73c73e94c3ccf4b85dccba0c4c3853fc76964d8d97cae4f4d62c021f72e422f49606c2519a43cdeb45e90b65f5110ca8859318c;ipb_member_id=1886830;ipb_pass_hash=3810cc5bb92e67242ab932646b6d9c56;ipb_session_id=41fc9128524537bbc6d51216b43bea3a;';
+exports.UserCookies = 'igneous=01d33ef3fdeb06d9cdc324f6b73c73e94c3ccf4b85dccba0c4c3853fc76964d8d97cae4f4d62c021f72e422f49606c2519a43cdeb45e90b65f5110ca8859318c;ipb_member_id=1886830;ipb_pass_hash=3810cc5bb92e67242ab932646b6d9c56;ipb_session_id=41fc9128524537bbc6d51216b43bea3a;';
 exports.UserCookie ='';
 exports.GallaryTypes = {
 	'doujinshi' : 'DOUJINSHI',
